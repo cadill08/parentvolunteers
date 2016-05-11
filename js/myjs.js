2b93b65894c822b6077b6168224d625f3c760d47
cadill08 <cadill08@gmail.com>
2016-05-11 23:39:33 +0000

    sixth update

--- js/myjs.js
+++ js/myjs.js
@@ -1,9 +1,33 @@
-$(document).ready(function () {
-    $("button").click(function () {
-        $("hours").animate({
-            height: 'toggle'
+ $('#heading').click(function () {
+     if ($('#content').is(':hidden')) {
+         $('#content').slideDown('slow');
+     } else {
+         $('#content').slideUp('slow');
+     }
+ });
 
-        });
+ $('#heading').click(function () {
+     if ($('#contentsmall').is(':hidden')) {
+         $('#contentsmall').slideDown('slow');
+     } else {
+         $('#contentsmall').slideUp('slow');
+     }
+ });
 
-    });
-});
+ $(function () {
+
+     $('.toggle').click(function (event) {
+         event.preventDefault();
+         var target = $(this).attr('href');
+         $(target).toggleClass('hidden show');
+     });
+
+ });
+
+ $('#box1').click(function () {
+     if ($("#box2").is(":hidden")) {
+         $("#box2").slideDown("slow");
+     } else {
+         $("#box2").slideUp("slow");
+     }
+ });
